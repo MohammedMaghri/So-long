b011ad9ff6e9b2e5ff8500db922fa9635c949f5c
Mohammed maghri <135870538+MohammedMaghri@users.noreply.github.com>
2024-01-03 19:41:59 +0100

    So Long Everything is Ready  :)

---
+++ Final So_long/mapchek.h
@@ -0,0 +1,145 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapchek.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaghri <mmaghri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/19 16:10:57 by mmaghri           #+#    #+#             */
+/*   Updated: 2024/01/03 19:37:51 by mmaghri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAPCHEK_H
+# define MAPCHEK_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "mapchek.h"
+# include <fcntl.h>
+# include <mlx.h>
+# include "mlx.h"
+
+typedef struct thefunctions
+{
+	int		worck;
+	int		movment;
+	int		monster_row;
+	int		monster_colum;
+	int		flag;
+	int		colom;
+	int		row;
+	int		index;
+	int		increment;
+	void	*forwindow;
+	void	*forinit;
+	char	*pic;
+	void	*picb;
+	void	*picc;
+	void	*picx;
+	void	*picz;
+	void	*picza;
+	int		x;
+	int		y;
+	int		xx;
+	int		yy;
+	char	**twod;
+	char	**tocopy;
+	int		yfor;
+	int		xfor;
+	int		indexfor;
+	char	**twodefor;
+	int		total;
+	int		col;
+	int		countcol;
+	int		countmovment;
+	char	*changedata;
+}	t_fac;
+
+typedef struct mapvlidity
+{
+	char	**tocpy;
+	char	*coll;
+	char	*wallptr;
+	char	*ground;
+	int		xx;
+	int		yy;
+	void	*windowptr;
+	char	*imageptr;
+	int		row ;
+	int		colum;
+	int		res ;
+	int		filed;
+	int		i;
+	char	**twode;
+	int		checkpoint ;
+	char	*allocation ;
+	int		flag ;
+	int		checker ;
+	int		index ;
+	int		player ;
+	int		collectiles ;
+	int		runout ;
+	int		numberofclomums ;
+	int		linelenght ;
+	int		checkinglinevalidity ;
+	int		totallenghtline ;
+	void	*ptrmlx;
+}	t_map;
+
+typedef struct moveit
+{
+	char	**doubled;
+}	t_move;
+
+int		movecoin(t_fac *me);
+int		w_check(char string);
+void	first_complete(int key, t_fac *me, int row, int colum);
+int		count_number_len(int number);
+void	paste(char string);
+void	fill(int number, int lenght, int index, char *string);
+void	move_player(int key, t_fac *me, int row, int colum);
+char	*change_data_type(int number);
+int		monster_move(t_fac *me);
+void	get_monster_position(t_fac *this, char **string);
+int		count_collectibles(char **string);
+void	thiskey_complete(int key, t_fac *me, int row, int colum);
+int		destro(char string, t_fac *this);
+int		check(char string);
+void	openthis(t_fac *me);
+void	put_character(t_fac *func, char **string);
+int		map_cle(int number, t_fac *copy);
+int		merge_for_moving_player(int key, t_fac *me);
+int		closeprogram(int key);
+char	**copy_of_array(char **string);
+void	fill_window_map(t_fac *func, char **string);
+char	*copy_of_string(char *string);
+char	**copymap(char *string);
+char	*thisfunctionresader(char *string, int fd, int size);
+void	fill_struct(t_fac *string, char **str);
+char	**return_array_from_text(int fd);
+char	**get_array(char *string);
+int		checkwalsinmap(char **string);
+int		maplastcheck(char **string);
+int		check_double_items(char **string);
+int		mergecheking(char **string, int numbercolum, int filed);
+void	trackleftside(char **string, int x, int y);
+void	printmapposition(char **string);
+int		checkrows(char *string);
+void	backtrack(char **string, int row, int colum);
+int		player_row_position(char **string);
+int		player_colum_position(char **string);
+int		weirddetection(char **string);
+int		maplenghtcheck(char **string);
+int		checkthelenghtline(char **array);
+int		checksidesofmap(char **string);
+int		checkvalidity(int player, int collec, int out);
+int		chekingarraybyone(char *string);
+int		chekthewalsinmap(char **string);
+int		lencount(char *string);
+char	**stringreturn(char *string);
+int		checkvalidmap(char *string);
+int		check_wierd_ithem(char **string);
+char	**merge_functions_checks(char *string, t_fac *me);
+#endif
---
+++ Final So_long/merge_chek.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge_chek.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaghri <mmaghri@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 13:57:15 by mmaghri           #+#    #+#             */
+/*   Updated: 2024/01/03 19:39:38 by mmaghri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mapchek.h"
+
+//check the raw that is in the colum if it actully 
+//accessible with the player
+int	checkrows(char *string)
+{
+	int	index ;
+
+	index = 0;
+	while (string[index])
+	{
+		if (string[index] == 'C')
+			return (-1);
+		index++ ;
+	}
+	return (0);
+}
+
+int	movecoin(t_fac *me)
+{
+	static int	flag ;
+
+	flag = 0;
+	if (me->worck == 0)
+		return (0);
+	me->picc = mlx_xpm_file_to_image(me->forinit, \
+	"col.xpm", &me->xx, &me->yy);
+	openthis(me);
+	me->picc = mlx_xpm_file_to_image(me->forinit, \
+	"col.xpm", &me->xx, &me->yy);
+	openthis(me);
+	flag++ ;
+	if (flag == 20)
+	{
+		me->picc = mlx_xpm_file_to_image(me->forinit, \
+		"lol.xpm", &me->xx, &me->yy);
+		openthis(me);
+		me->picc = mlx_xpm_file_to_image(me->forinit, \
+		"lol.xpm", &me->xx, &me->yy);
+		openthis(me);
+		flag = 0;
+	}
+	return (0);
+}
+
+int	mergecheking(char **string, int numbercolum, int filed)
+{
+	t_map	merge;
+
+	merge.res = checkwalsinmap(string);
+	if (merge.res == -1)
+		return (-1);
+	merge.i = checksidesofmap(string);
+	if (merge.i == -1)
+		return (-1);
+	merge.index = check_double_items(string);
+	if (merge.index == -1)
+		return (-1);
+	backtrack(string, numbercolum, filed);
+	merge.flag = maplastcheck(string);
+	if (merge.flag == -1)
+		return (-1);
+	return (0);
+}
+
+//this function just show live visaliwe of the map
+void	printmapposition(char **string)
+{
+	t_map	printmappostion ;
+
+	printmappostion.index = 0;
+	printmappostion.linelenght = maplenghtcheck(string);
+	while (printmappostion.index < printmappostion.linelenght)
+	{
+		printf("%s\n", string[printmappostion.index]);
+		printmappostion.index++ ;
+	}
+	printf("\n");
+}
+
+//this is the function that check is all the move are valid 
+//For the player
+void	backtrack(char **string, int x, int y)
+{
+	t_map		backtrace;
+
+	string[x][y] = 'F';
+	backtrace.linelenght = maplenghtcheck(string) - 1;
+	if ((y - 1) >= 0 && string[x][y - 1] \
+	!= '1' && string[x][y - 1] != 'F' && string[x][y - 1] != 'E')
+		backtrack(string, x, y - 1);
+	if (x < backtrace.linelenght && string[x + 1][y] != '1' && \
+	string[x + 1][y] != 'F' && string[x + 1][y] != 'E')
+		backtrack(string, x + 1, y);
+	if ((y + 1) < lencount(string[x]) && \
+	string[x][y + 1] != '1' && string[x][y + 1] != 'F' && \
+	string[x][y + 1] != 'E')
+		backtrack(string, x, y + 1);
+	if ((x - 1) >= 0 && string[x - 1][y] != '1' \
+	&& string[x - 1][y] != 'F' && string[x - 1][y] != 'E')
+		backtrack(string, x - 1, y);
+}
